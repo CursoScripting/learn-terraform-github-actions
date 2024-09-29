c743a8f35a7bc38dfe0fdb640ad3f27a08f89d9e
brqx <r23@mailz.es>
2024-09-29 10:47:33 +0200

    The most simple pipeline

--- main.tf
+++ main.tf
@@ -35,21 +35,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# resource "aws_instance" "web" {
-#   ami                    = data.aws_ami.ubuntu.id
-#   instance_type          = "t3.micro"
-#   vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               apt-get update
-#               apt-get install -y apache2
-#               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-#               echo "Hello World" > /var/www/html/index.html
-#               systemctl restart apache2
-#               EOF
-# }
-
 resource "aws_security_group" "web-sg" {
   name = "web-brqx-sg"
   ingress {
@@ -69,7 +54,7 @@ resource "aws_security_group" "web-sg" {
 
 output "web-address" {
 #  value = "${aws_instance.web.public_dns}:8080"
-  value = "https://pepito.com:8080"
+  value = "https://anysite.com:8080"
 
 }
 
